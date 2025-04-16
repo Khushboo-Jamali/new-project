789d75e80f382326fdb826b3a4be29abb3e36a33
khushboo-jamali <jamalikhushboo@gmail.com>
2025-04-16 17:19:39 +0500

    React functional and class based component

---
+++ src/ClassComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ClassComonent extends Component {
+  render() {
+    return <h1>Hello world, this is my first class-based component</h1>;
+  }
+}
+
+export default ClassComonent;
---
+++ src/FuncComponent.js
@@ -0,0 +1,4 @@
+export function FuncComponent() {
+  return <h1>This is my first function based component </h1>;
+}
+// export default FuncComponent;
